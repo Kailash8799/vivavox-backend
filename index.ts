776d9d8385754aecbcab1d50cea65ff314e7229c
Kailash8799 <kailashrajput8799@gmail.com>
2024-01-22 00:58:20 +0530

    added get profile method

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import forgotModule from './routes/users/forgotpassword'
 import verifyuserModule from './routes/users/verifyuser'
 import contactuserModule from './routes/users/contact'
 import editprofileModule from './routes/users/editprofile'
+import getprofileModule from './routes/users/getprofile'
 import welcomeModule from './routes/default/welcome'
 import connectDb from "./middleware/_db";
 
@@ -28,6 +29,7 @@ app.use("/v1/users/forgot", forgotModule);
 app.use("/v1/users/verifyemail", verifyuserModule);
 app.use("/v1/users/contact", contactuserModule);
 app.use("/v1/users/editprofile", editprofileModule);
+app.use("/v1/users/getprofile", getprofileModule);
 
 server.listen(PORT, () => {
     console.log(`Server is ready ${PORT}`);
---
+++ routes/users/getprofile.ts
@@ -0,0 +1,37 @@
+require('dotenv').config()
+import express from 'express'
+import jwt, { JwtPayload } from "jsonwebtoken";
+import Profile from '../../models/Profile';
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+    try {
+        if (req.method !== "POST") {
+            res.json({ success: false, message: "Some error occured!" });
+            return;
+        }
+        const { token } = req.body;
+        if (token === undefined || token === null || token === "") {
+            res.json({ success: false, message: "token not valid" });
+            return;
+        }
+        const { email } = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+        if (email === undefined) {
+            res.json({ success: false, message: "token not valid" });
+            return;
+        }
+        const userprofile = await Profile.findOne({ email });
+        if (!userprofile) {
+            res.json({ success: false, message: "token is not valid" });
+            return;
+        }
+        res.json({ success: true, message: "Profile",profile:userprofile }).status(200);
+        return;
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "token is invalid" }).status(401);
+        return;
+    }
+})
+
+export default router
