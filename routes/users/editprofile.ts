050f22e72bf843d2a7f32314ce4e93798ac4fc78
Kailash8799 <kailashrajput8799@gmail.com>
2024-01-24 23:42:40 +0530

    added get updated value

--- routes/users/editprofile.ts
+++ routes/users/editprofile.ts
@@ -1,6 +1,5 @@
 require('dotenv').config()
 import express from 'express'
-import User from '../../models/User';
 import Profile from '../../models/Profile';
 import { queryUpdateData } from '../../constants/methods/queryupdatedata';
 const router = express.Router();
@@ -15,17 +14,19 @@ router.post("/", async (req, res) => {
         }
         const { profile, email } = req.body;
         const query = await queryUpdateData(profile);
-        const olduser = await Profile.findOne({email});
+        const olduser = await Profile.findOne({ email });
         if (olduser === null || olduser === undefined) {
             res.json({ success: false, message: "Invalid session please logout and login again!" });
             return;
         }
-        const u = await Profile.findOneAndUpdate({ email }, query);
+        const u = await Profile.findOneAndUpdate({ email }, query, {
+            new: true,
+        });
         if (!u) {
             res.json({ success: false, message: "Some error occured updating profile!" });
             return;
         }
-        res.json({ success: true, message: "Profile updated successfully",profile:u }).status(200);
+        res.json({ success: true, message: "Profile updated successfully", profile: u }).status(200);
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Some error occured!" });
