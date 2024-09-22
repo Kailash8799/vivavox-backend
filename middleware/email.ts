baeb8b6e4a2874562443644fdd1a644c26d47d74
Kailash8799 <kailashrajput8799@gmail.com>
2024-09-22 13:46:49 +0530

    change email sender

--- middleware/email.ts
+++ middleware/email.ts
@@ -5,6 +5,7 @@ import { SendMailProps } from '../constants/props/email';
 const sendMail = async ({ to_email, htmlemail, subject }: SendMailProps) => {
 
     try {
+        console.log(process.env.EMAIL);
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
             port: 465,
@@ -15,43 +16,50 @@ const sendMail = async ({ to_email, htmlemail, subject }: SendMailProps) => {
             },
         });
 
-        var mailOptions = {
+        let mailOptions = {
             from: process.env.EMAIL,
             to: to_email,
             subject: subject,
             html: htmlemail,
         };
 
-        const server = await new Promise((resolve, reject) => {
-            // verify connection configuration
-            transporter.verify(function (error, success) {
-                if (success) {
-                    resolve(success);
-                }
-                reject(error);
-            });
-        });
-        if (!server) {
-            console.log("server : " + server);
+        // const server = await new Promise((resolve, reject) => {
+        //     // verify connection configuration
+        //     transporter.verify(function (error, success) {
+        //         if (success) {
+        //             resolve(success);
+        //         }
+        //         reject(error);
+        //     });
+        // });
+        // if (!server) {
+        //     console.log("server : " + server);
+        //     return false;
+        // }
+
+        // const success = await new Promise((resolve, reject) => {
+        //     // send mail
+        //     transporter.sendMail(mailOptions).then((info, err) => {
+        //         if (info.response.includes("250")) {
+        //             resolve(true);
+        //         }
+        //         console.log("***")
+        //         console.log(err)
+        //         reject(err);
+        //     });
+        // });
+
+        // if (!success) {
+        //     return false;
+        // }
+        // return true;
+        try {
+            await transporter.sendMail(mailOptions);
+            return true;
+        } catch (error) {
+            console.error("Error sending verification email:", error);
             return false;
         }
-
-        const success = await new Promise((resolve, reject) => {
-            // send mail
-            transporter.sendMail(mailOptions).then((info, err) => {
-                if (info.response.includes("250")) {
-                    resolve(true);
-                }
-                console.log("***")
-                console.log(err)
-                reject(err);
-            });
-        });
-
-        if (!success) {
-            return false;
-        }
-        return true;
 
 
     } catch (error) {
